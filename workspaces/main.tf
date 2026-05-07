7fbbd3e6470259c99660d99c9330701af5b3034d
mudasirmattoo <mudasirmmattoo@gmail.com>
2026-05-07 19:05:47 +0530

    key pair added

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -17,8 +17,11 @@ variable "instance_type" {
   }
 }
 
+
+
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami = var.ami
   instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+
 }
--- workspaces/modules/ec2_instance/main.tf
+++ workspaces/modules/ec2_instance/main.tf
@@ -10,10 +10,16 @@ variable "instance_type" {
   description = "This is the instance type, for example: t2.micro"
 }
 
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-tf-key"
+  public_key = file("/home/mudasirmattoo/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "example" {
     ami = var.ami
     instance_type = var.instance_type
-
+    key_name = aws_key_pair.deployer.key_name
     tags = {
       Name = "vault-instance"
     }
